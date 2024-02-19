69dee2bbd95151dc37d33111b7f389fbd5b67679
Damith Premasiri <damithpremasiri@gmail.com>
2024-02-19 18:37:21 +0000

    test

--- experiments/eval_llm_native.py
+++ experiments/eval_llm_native.py
@@ -16,28 +16,29 @@ dataset = dataset.to_pandas()
 print('dataset loading finished')
 
 # testing
-# dataset = dataset[100:102]
-# prompt_list = generate_prompts(dataset)
+dataset = dataset[100:102]
+prompt_list = generate_prompts(dataset)
+prompt_list = [f'<s>[INST] {prompt} [/INST]'for prompt in prompt_list]
 
-while(True):
-    prompt = input("input : ")
-    prompt_list = [prompt]
+# while(True):
+#     prompt = input("input : ")
+#     prompt_list = [prompt]
 
-    generation_config = GenerationConfig(
-        max_new_tokens=10, do_sample=True, top_k=10, eos_token_id=model.config.eos_token_id,
-        pad_token_id=model.config.eos_token_id, temperature=0.7,
-        num_return_sequences=10, torch_dtype=torch.bfloat16,
-    )
+generation_config = GenerationConfig(
+    max_new_tokens=10, do_sample=True, top_k=10, eos_token_id=model.config.eos_token_id,
+    pad_token_id=model.config.eos_token_id, temperature=0.7,
+    num_return_sequences=10, torch_dtype=torch.bfloat16,
+)
 
-    encoding = tokenizer(prompt_list, padding=True, truncation=False, return_tensors="pt").to(model.device)
-    outputs = model.generate(input_ids=encoding.input_ids, attention_mask=encoding.attention_mask,
-                             generation_config=generation_config)
-    detach = outputs.detach().cpu().numpy()
-    outputs = detach.tolist()
-    out_list = []
-    out_list.extend([tokenizer.decode(out, skip_special_tokens=True) for out in outputs])
+encoding = tokenizer(prompt_list, padding=True, truncation=False, return_tensors="pt").to(model.device)
+outputs = model.generate(input_ids=encoding.input_ids, attention_mask=encoding.attention_mask,
+                         generation_config=generation_config)
+detach = outputs.detach().cpu().numpy()
+outputs = detach.tolist()
+out_list = []
+out_list.extend([tokenizer.decode(out, skip_special_tokens=True) for out in outputs])
 
-    print([tokenizer.decode(out, skip_special_tokens=True) for out in outputs])
+# print([tokenizer.decode(out, skip_special_tokens=True) for out in outputs])
 
-    with open('raw_results_from_model.txt', 'w') as f:
-        f.writelines("\n".join(out_list))
+with open('raw_results_from_model.txt', 'w') as f:
+    f.writelines("\n".join(out_list))
