324b461be5bd47a75699e410d504be29fa2944bd
Damith Premasiri <damithpremasiri@gmail.com>
2024-02-19 17:18:19 +0000

    test

--- data/prompts/single_prompts.py
+++ data/prompts/single_prompts.py
@@ -4,113 +4,90 @@ string_templates = {
 
     'age':
         """
-        Irony is a literary or rhetorical device in which there is a discrepancy between what is said and what is meant, 
-        or between expectations and reality. It often involves a contrast between appearance and reality, 
-        or a situation in which the outcome is the opposite of what one might expect.
-        Imagine that you are a {age} years old person. Following is a message and its' reply from a conversation. 
-        
-        Message : {message}
-        Reply   : {reply}
-        
-        Do you find the above reply ironic? Answer Yes/No
+        Irony is a literary or rhetorical device in which there is a discrepancy between what is said and what is meant,\n 
+        or between expectations and reality. It often involves a contrast between appearance and reality,\n 
+        or a situation in which the outcome is the opposite of what one might expect.\n
+        Imagine that you are a {age} years old person. Following is a message and its' reply from a conversation.\n 
+        Message : {message}\n
+        Reply   : {reply}\n
+        Do you find the above reply ironic? Answer Yes/No\n
         """
     ,
     'sex':
         """
-        Irony is a literary or rhetorical device in which there is a discrepancy between what is said and what is meant, 
-        or between expectations and reality. It often involves a contrast between appearance and reality, 
-        or a situation in which the outcome is the opposite of what one might expect.
-        
-        Imagine that you are a {sex}. Following is a message and its' reply from a conversation. 
-        
-        Message : {message}
-        Reply   : {reply}
-        
-        Do you find the above reply ironic? Answer Yes/No
+        Irony is a literary or rhetorical device in which there is a discrepancy between what is said and what is meant,\n 
+        or between expectations and reality. It often involves a contrast between appearance and reality,\n 
+        or a situation in which the outcome is the opposite of what one might expect.\n
+        Imagine that you are a {sex}. Following is a message and its' reply from a conversation.\n 
+        Message : {message}\n
+        Reply   : {reply}\n
+        Do you find the above reply ironic? Answer Yes/No\n
         """
     ,
     'ethnicity':
         """
-        Irony is a literary or rhetorical device in which there is a discrepancy between what is said and what is meant, 
-        or between expectations and reality. It often involves a contrast between appearance and reality, 
-        or a situation in which the outcome is the opposite of what one might expect.
-
-        Imagine that you have a {ethnicity} ethnic background. Following is a message and its' reply from a conversation. 
-
-        Message : {message}
-        Reply   : {reply}
-
-        Do you find the above reply ironic? Answer Yes/No
+        Irony is a literary or rhetorical device in which there is a discrepancy between what is said and what is meant,\n 
+        or between expectations and reality. It often involves a contrast between appearance and reality,\n 
+        or a situation in which the outcome is the opposite of what one might expect.\n
+        Imagine that you have a {ethnicity} ethnic background. Following is a message and its' reply from a conversation.\n 
+        Message : {message}\n
+        Reply   : {reply}\n
+        Do you find the above reply ironic? Answer Yes/No\n
         """
     ,
     'country_of_birth':
         """
-        Irony is a literary or rhetorical device in which there is a discrepancy between what is said and what is meant, 
-        or between expectations and reality. It often involves a contrast between appearance and reality, 
-        or a situation in which the outcome is the opposite of what one might expect.
-
-        Imagine that you were born in {country_of_birth}. Following is a message and its' reply from a conversation. 
-
-        Message : {message}
-        Reply   : {reply}
-
-        Do you find the above reply ironic? Answer Yes/No
+        Irony is a literary or rhetorical device in which there is a discrepancy between what is said and what is meant,\n 
+        or between expectations and reality. It often involves a contrast between appearance and reality,\n 
+        or a situation in which the outcome is the opposite of what one might expect.\n
+        Imagine that you were born in {country_of_birth}. Following is a message and its' reply from a conversation.\n 
+        Message : {message}\n
+        Reply   : {reply}\n
+        Do you find the above reply ironic? Answer Yes/No\n
         """
     ,
     'country_of_residence':
         """
-        Irony is a literary or rhetorical device in which there is a discrepancy between what is said and what is meant, 
-        or between expectations and reality. It often involves a contrast between appearance and reality, 
-        or a situation in which the outcome is the opposite of what one might expect.
-
-        Imagine that you are a resident in {country_of_residence}. Following is a message and its' reply from a conversation. 
-
-        Message : {message}
-        Reply   : {reply}
-
-        Do you find the above reply ironic? Answer Yes/No
+        Irony is a literary or rhetorical device in which there is a discrepancy between what is said and what is meant,\n 
+        or between expectations and reality. It often involves a contrast between appearance and reality,\n 
+        or a situation in which the outcome is the opposite of what one might expect.\n
+        Imagine that you are a resident in {country_of_residence}. Following is a message and its' reply from a conversation.\n 
+        Message : {message}\n
+        Reply   : {reply}\n
+        Do you find the above reply ironic? Answer Yes/No\n
         """
     ,
     'nationality':
         """
-        Irony is a literary or rhetorical device in which there is a discrepancy between what is said and what is meant, 
-        or between expectations and reality. It often involves a contrast between appearance and reality, 
-        or a situation in which the outcome is the opposite of what one might expect.
-
-        Imagine that you are a national of {nationality}. Following is a message and its' reply from a conversation. 
-
-        Message : {message}
-        Reply   : {reply}
-
-        Do you find the above reply ironic? Answer Yes/No
+        Irony is a literary or rhetorical device in which there is a discrepancy between what is said and what is meant,\n 
+        or between expectations and reality. It often involves a contrast between appearance and reality,\n 
+        or a situation in which the outcome is the opposite of what one might expect.\n
+        Imagine that you are a national of {nationality}. Following is a message and its' reply from a conversation.\n 
+        Message : {message}\n
+        Reply   : {reply}\n
+        Do you find the above reply ironic? Answer Yes/No\n
         """
     ,
     'is_student':
         """
-        Irony is a literary or rhetorical device in which there is a discrepancy between what is said and what is meant, 
-        or between expectations and reality. It often involves a contrast between appearance and reality, 
-        or a situation in which the outcome is the opposite of what one might expect.
-
-        Imagine that you are {is_student} student. Following is a message and its' reply from a conversation. 
-
-        Message : {message}
-        Reply   : {reply}
-
-        Do you find the above reply ironic? Answer Yes/No
+        Irony is a literary or rhetorical device in which there is a discrepancy between what is said and what is meant,\n 
+        or between expectations and reality. It often involves a contrast between appearance and reality,\n 
+        or a situation in which the outcome is the opposite of what one might expect.\n
+        Imagine that you are {is_student} student. Following is a message and its' reply from a conversation.\n 
+        Message : {message}\n
+        Reply   : {reply}\n
+        Do you find the above reply ironic? Answer Yes/No\n
         """
     ,
     'employment_mode':
         """
-        Irony is a literary or rhetorical device in which there is a discrepancy between what is said and what is meant, 
-        or between expectations and reality. It often involves a contrast between appearance and reality, 
-        or a situation in which the outcome is the opposite of what one might expect.
-
-        Imagine that you are {employment_mode} employed. Following is a message and its' reply from a conversation. 
-
-        Message : {message}
-        Reply   : {reply}
-
-        Do you find the above reply ironic? Answer Yes/No
+        Irony is a literary or rhetorical device in which there is a discrepancy between what is said and what is meant,\n 
+        or between expectations and reality. It often involves a contrast between appearance and reality,\n 
+        or a situation in which the outcome is the opposite of what one might expect.\n
+        Imagine that you are {employment_mode} employed. Following is a message and its' reply from a conversation.\n 
+        Message : {message}\n
+        Reply   : {reply}\n
+        Do you find the above reply ironic? Answer Yes/No\n
         """
 }
 
