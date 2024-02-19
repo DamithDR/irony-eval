d66497a9a66d45ab151e076441127c3cf0b4b511
Damith Premasiri <damithpremasiri@gmail.com>
2024-02-19 16:45:14 +0000

    test

---
+++ experiments/eval_llm_native.py
@@ -0,0 +1,31 @@
+# Load model directly
+from datasets import load_dataset
+from transformers import AutoTokenizer, AutoModelForCausalLM, GenerationConfig
+
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1")
+model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1")
+
+dataset = load_dataset('Multilingual-Perspectivist-NLU/EPIC', split='train')
+dataset = dataset.to_pandas()
+print('dataset loading finished')
+
+# testing
+dataset = dataset[300:305]
+
+data_batch = dataset.tolist()
+
+generation_config = GenerationConfig(
+    max_new_tokens=2, do_sample=True, top_k=1, top_p=0.95, eos_token_id=model.config.eos_token_id,
+    num_return_sequences=1
+)
+
+encoding = tokenizer(data_batch, padding=True, truncation=False, return_tensors="pt").to(model.device)
+outputs = model.generate(input_ids=encoding.input_ids, attention_mask=encoding.attention_mask,
+                         generation_config=generation_config)
+detach = outputs.detach().cpu().numpy()
+outputs = detach.tolist()
+out_list = []
+out_list.extend([tokenizer.decode(out, skip_special_tokens=True) for out in outputs])
+
+with open('raw_results_from_model.txt', 'w') as f:
+    f.writelines("\n".join(out_list))
