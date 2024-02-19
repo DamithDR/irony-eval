81f1fda776da5b67f9ddf7148e3e9094fe6c99b5
Damith Premasiri <damithpremasiri@gmail.com>
2024-02-19 16:55:24 +0000

    test

--- experiments/eval_llm_native.py
+++ experiments/eval_llm_native.py
@@ -20,6 +20,7 @@ prompt_list = generate_prompts(dataset)
 
 generation_config = GenerationConfig(
     max_new_tokens=2, do_sample=True, top_k=50, eos_token_id=model.config.eos_token_id,
+    pad_token_id=model.config.eos_token_id, temperature=0.2,
     num_return_sequences=1
 )
 
