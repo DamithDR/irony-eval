713adf7f5216a18b778f6da3beaea005757a8536
Damith Premasiri <damithpremasiri@gmail.com>
2024-02-19 17:35:15 +0000

    test

--- experiments/eval_llm_native.py
+++ experiments/eval_llm_native.py
@@ -16,22 +16,28 @@ dataset = dataset.to_pandas()
 print('dataset loading finished')
 
 # testing
-dataset = dataset[300:302]
-prompt_list = generate_prompts(dataset)
+# dataset = dataset[300:302]
+# prompt_list = generate_prompts(dataset)
 
-generation_config = GenerationConfig(
-    max_new_tokens=50, do_sample=True, top_k=50, eos_token_id=model.config.eos_token_id,
-    pad_token_id=model.config.eos_token_id, temperature=0.2,
-    num_return_sequences=10,torch_dtype=torch.bfloat16,
-)
+while(True):
+    prompt = input("input : ")
+    prompt_list = [prompt]
 
-encoding = tokenizer(prompt_list, padding=True, truncation=False, return_tensors="pt").to(model.device)
-outputs = model.generate(input_ids=encoding.input_ids, attention_mask=encoding.attention_mask,
-                         generation_config=generation_config)
-detach = outputs.detach().cpu().numpy()
-outputs = detach.tolist()
-out_list = []
-out_list.extend([tokenizer.decode(out, skip_special_tokens=True) for out in outputs])
+    generation_config = GenerationConfig(
+        max_new_tokens=10, do_sample=True, top_k=50, eos_token_id=model.config.eos_token_id,
+        pad_token_id=model.config.eos_token_id, temperature=0.2,
+        num_return_sequences=1,torch_dtype=torch.bfloat16,
+    )
 
-with open('raw_results_from_model.txt', 'w') as f:
-    f.writelines("\n".join(out_list))
+    encoding = tokenizer(prompt_list, padding=True, truncation=False, return_tensors="pt").to(model.device)
+    outputs = model.generate(input_ids=encoding.input_ids, attention_mask=encoding.attention_mask,
+                             generation_config=generation_config)
+    detach = outputs.detach().cpu().numpy()
+    outputs = detach.tolist()
+    out_list = []
+    out_list.extend([tokenizer.decode(out, skip_special_tokens=True) for out in outputs])
+
+    print([tokenizer.decode(out, skip_special_tokens=True) for out in outputs])
+
+    with open('raw_results_from_model.txt', 'w') as f:
+        f.writelines("\n".join(out_list))
