7b8c895545ee78c1ce91f1eb1601bdaebdc94b18
Damith Premasiri <damithpremasiri@gmail.com>
2024-02-19 18:45:16 +0000

    test

--- experiments/eval_llm_native.py
+++ experiments/eval_llm_native.py
@@ -25,9 +25,9 @@ prompt_list = [f'<s>[INST] {prompt} [/INST]'for prompt in prompt_list]
 #     prompt_list = [prompt]
 
 generation_config = GenerationConfig(
-    max_new_tokens=10, do_sample=True, top_k=10, eos_token_id=model.config.eos_token_id,
-    pad_token_id=model.config.eos_token_id, temperature=0.7,
-    num_return_sequences=10, torch_dtype=torch.bfloat16,
+    max_new_tokens=2, do_sample=True, top_k=2, eos_token_id=model.config.eos_token_id,
+    pad_token_id=model.config.eos_token_id, temperature=0.2,
+    num_return_sequences=1, torch_dtype=torch.bfloat16,
 )
 
 encoding = tokenizer(prompt_list, padding=True, truncation=False, return_tensors="pt").to(model.device)
